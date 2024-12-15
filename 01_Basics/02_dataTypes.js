97f4d4a68ac1f1f65b1199221406e83eea11d6aa
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-15 06:22:45 +0000

    Data Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+// "use strict"; // treat all JS code as newer version
+// alert("hello")  we are using Node Js not Brouser so this will give error
+
+let name = "Santosh"
+let age= 16
+let isLoggedIn = false
+// Number ==> 2 to power 53
+//bigint
+//boolean ==> true/false
+//null==>standalone value
+//undefined ==> 
+//symbol ==>unique
+
+
+// object 
+
+console.log(typeof "hitesh")
