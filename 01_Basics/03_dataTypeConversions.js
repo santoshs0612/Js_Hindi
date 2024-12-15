6ce11d0d450f864a5a57f5134e91dea21d889e52
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-15 06:33:39 +0000

    Conversion Of Data Types

---
+++ 01_Basics/03_dataTypeConversions.js
@@ -0,0 +1,31 @@
+let score = "33a"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33"  ===> 33
+// "33adf" ===> NaN
+// true ==> 1; false ==>0
+
+let isLoggedIn = "Santos"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1===> true, 0==>false
+
+// "" ==>false , 
+// "Santa" ==> true;
+
+
+let someNumber = 333
+let stringNumber  = String(someNumber)
+console.log(typeof(stringNumber))
+console.log(someNumber)
