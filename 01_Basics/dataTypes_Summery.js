bdf03f9584ceb2d289562ffc45ab782055da7581
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-16 11:02:07 +0000

    Stack and Heap memory in JS

--- 01_Basics/dataTypes_Summery.js
+++ 01_Basics/dataTypes_Summery.js
@@ -11,10 +11,10 @@
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id===anotherId)// Not Same 
+//console.log(id===anotherId)// Not Same 
 
 const bigNumber  = 34567656454534343212999n
-console.log(typeof(bigNumber))
+//console.log(typeof(bigNumber))
 
 
 // Reference Type (Non Premetive)
@@ -28,8 +28,34 @@ let myObj = {
     age: 22,
 }
 
-const myFunction = function(){
-    console.log("Hello World")
-}
+// const myFunction = function(){
+//     console.log("Hello World")
+// }
 
-console.log(typeof(myFunction))
+// console.log(typeof(myFunction))
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack(Premitive)  Heap (Non-Primitive)
+
+let email = "santosh8@gmail.com"
+
+let emalNew = email
+
+console.log(emalNew)
+
+
+let userOne={
+    email: "useremail.com",
+    upi: "user1.ybl",
+} 
+
+let userTwo = userOne;
+userTwo.email="santos.0745@gmai.com"
+
+console.log(userTwo.email)
+console.log(userOne.email)
