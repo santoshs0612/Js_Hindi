b8fce29f1cea844be0e91ee2993dc7127a9acdb5
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-16 11:53:08 +0000

    Operation on Number and Maths in Js

---
+++ 01_Basics/06_numsAndMaths.js
@@ -0,0 +1,40 @@
+const score= 4000
+
+// console.log(score)
+
+const balance = new Number(100)
+
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.89303
+// priorites are on before decimals 
+// console.log(otherNumber.toPrecision(3));
+
+const hundereds = 10000000
+// console.log(hundereds.toLocaleString('en-IN'))
+
+//  ++++++++++++++++++++Maths ++++++++++++++++++++++++
+
+
+// console.log(Math)
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.6))
+
+// console.log(Math.min(4,3,6,7,0))
+
+// console.log(Math.random());
+
+
+// console.log((Math.random()*10)+1)
+
+const min =10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max-min+1)) + min))
