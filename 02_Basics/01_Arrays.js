81434f19d32ea421897ad6b7607ade88d01c5c87
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-16 15:56:49 +0000

    Array Part1 declation and functions methods

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,46 @@
+
+// arrays
+
+const myArray = [0,1,3,4,5]
+
+console.log(myArray[0])
+
+let arr = new Array(1,3,4,5,56,78,9)
+console.log(arr[0])
+
+// Array Methods 
+
+// myArray.push(6)
+// console.log(myArray)
+// myArray.push(9)
+// console.log(myArray)
+// myArray.pop()
+// console.log(myArray)
+
+// myArray.unshift(9)
+
+// myArray.shift()
+// myArray.shift()
+// console.log(myArray)
+
+// console.log(myArray.includes(0))
+
+// console.log(myArray.indexOf(10))
+
+// const newArr = myArray.join()
+// console.log(myArray)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// Slice , Plice
+
+console.log("A " ,myArray);
+const myn1= myArray.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArray)
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+console.log("C ", myArray)
+
+
