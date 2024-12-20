b5f692a8ead8a327ec860d579e06ebfcc426e59d
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-20 12:12:11 +0000

    switch Case

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,52 @@
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("May")
+        break;
+    case 5:
+        console.log("June")
+        break;
+    case 6:
+        console.log("July")
+        break;
+    case 7:
+        console.log("Aug")
+        break;
+    case 8:
+        console.log("January")
+        break;
+    case 1:
+        console.log("January")
+        break;
+    case 1:
+        console.log("January")
+        break;
+    case 1:
+        console.log("January")
+        break;
+    case 1:
+        console.log("January")
+        break;
+    default:
+        console.log("Enter corrent Month")
+        break;
+}
