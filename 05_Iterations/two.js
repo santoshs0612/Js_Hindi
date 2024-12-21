d04d1196168c6f61679eb5d349fc443eb12bf811
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-21 06:48:11 +0000

    while and do while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,25 @@
+
+// while loops
+
+// let index =0
+// while (index <= 10) {
+//     console.log(`Value of index id ${index}`)
+//     index= index+2
+// }
+
+let myArray = ["Flash", "BatMan", "SuperMan"]
+
+let index=0
+
+// while (index< myArray.length) {
+//     console.log(`Value id ${myArray[index]}`)
+//     index= index+1
+// }
+
+
+let scoe = 11;
+
+do {
+    console.log(`Socre is ${scoe}`)
+    scoe++;
+} while (scoe<=10);
