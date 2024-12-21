aac4ead022e69a2058726acab0b895971eea5b93
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2024-12-21 10:10:10 +0000

    for in loops

---
+++ 05_Iterations/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js :"JavaScript",
+    cpp:"C++",
+    rb:"ruby",
+    swift: "Swift bt apple"
+}
+
+for (const key in myObject) {
+    // console.log(myObject[key])
+    // console.log(`${key} sortCut is for  ${myObject[key]}`)
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    // console.log(key);
+    // console.log(programming[key]);
+}
+
+const map = new Map()
+
+map.set('In',"India")
+map.set('Fr',"France")
+map.set("Pk","Pakistan")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
